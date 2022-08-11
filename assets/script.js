524eb76203d95da10b966f5567f70fd82011fa97
Lynchk07 <lynch.kristinj@gmail.com>
2022-08-10 23:11:36 -0700

    parsinjawson

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,23 @@
 //}
 //});
 
+var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+
+var weatherData = document.getElementById ("#weather-icons")
+
 fetch(requestUrl)
 .then(function(response){
+    //converts to JSON object
     return response.json();
 })
+
+.then(function(data) {
+    //display data in HTML
+    var weatherArray = data.response.doc;
+    for (let i = 0; i < array.length; i++) {
+        var listForecastFive = document.createElement("li");
+        listForecastFive.textContent = weatherArray[i].description;
+        weatherData.appendChild(listForecastFive);
+        
+    }
+});
