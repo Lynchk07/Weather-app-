8e206d6506ce8897827bcfc38b12b823ba9eb7a2
Lynchk07 <lynch.kristinj@gmail.com>
2022-08-11 22:38:37 -0700

    thweather

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,32 @@
-//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
-//$.ajax({
-   // url: 'https://randomuser.me/api/',
-//dataType: 'json',
-//success: function(data) {
-//console.log(data);
-//}
-//});
 
-var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+//var weatherData = document.getElementById ("#weather-icons")
 
-var weatherData = document.getElementById ("#weather-icons")
+var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname},${statecode},${countrycode}&limit=${limit}&lat=${lat}&lon=${lon}&exclude=${part}&appid=62645b864f757d8e71f84817791c80ba`
 
 fetch(requestUrl)
 .then(function(response){
     //converts to JSON object
     return response.json();
+
 })
 
 .then(function(data) {
-    //display data in HTML
-    var weatherArray = data.response.doc;
-    for (let i = 0; i < array.length; i++) {
-        var listForecastFive = document.createElement("li");
-        listForecastFive.textContent = weatherArray[i].description;
-        weatherData.appendChild(listForecastFive);
-        
-    }
+    console.log(data);
+    requestUrl = data.cityname;
+    console.log("cityname[0]")
 });
+
+then(function(cityname){
+    console.log(city);
+  
+});
+    //display data in HTMl
+    //weatherArray = data.response.doc;
+    //for (let i = 0; i < array.//length; i++) {
+       //var listForecastFive = //document.createElement("li");
+        //listForecastFive.textContent = weatherArray[i].description;
+        //weatherData.appendChild(listForecastFive);
+        
+   // }
+//});
